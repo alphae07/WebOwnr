722e0f27960d0be9176f81dac782a2013bc582be
alphae07 <128516952+alphae07@users.noreply.github.com>
2025-12-04 16:06:34 +0100

    Update page.tsx

--- app/(main)/dashboard/editor/[siteSlug]/page.tsx
+++ app/(main)/dashboard/editor/[siteSlug]/page.tsx
@@ -1,7 +1,7 @@
 // app/dashboard/editor/[siteSlug]/page.tsx
 import EditorClient from "@/components/EditorClient";
 
-export default function Page({ params }: { params: { siteSlug: string } }) {
+export default function Page({ params }: any) {
   const { siteSlug } = params;
   // Keep page as a server component and hand the slug down to the client component
   return <EditorClient siteSlug={siteSlug} />;

04005ab7f68d9eb088717d9732870cee941c0f67
alphae07 <128516952+alphae07@users.noreply.github.com>
2025-12-04 16:12:10 +0100

    Update page.tsx

--- app/(sites)/[site]/[...catchall]/page.tsx
+++ app/(sites)/[site]/[...catchall]/page.tsx
@@ -1,16 +1,12 @@
 import { getSiteBySlug } from "@/lib/getSiteData";
 import { renderSite } from "@/lib/render";
 
-export default async function Page({
-  params,
-}: {
-  params: { site: string; catchall?: string[] };
-}) {
-  const site = await getSiteBySlug(params.site);
+export default async function Page({ params }) {
+  const { site, catchall } = params;
 
-  if (!site) return <div>Site not found</div>;
+  const siteData = await getSiteBySlug(site);
+  if (!siteData) return <div>Site not found</div>;
 
-  const path = params.catchall?.join("/") || "home";
-
-  return renderSite(site, path);
+  const path = catchall?.join("/") || "home";
+  return renderSite(siteData, path);
 }

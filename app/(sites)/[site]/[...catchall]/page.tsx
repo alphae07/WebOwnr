96a8ae57eea3bfb71569e56644ea1ad280f079d7
alphae07 <128516952+alphae07@users.noreply.github.com>
2025-12-04 16:57:11 +0100

    Update page.tsx

--- app/(sites)/[site]/[...catchall]/page.tsx
+++ app/(sites)/[site]/[...catchall]/page.tsx
@@ -1,4 +1,3 @@
-// app/(sites)/[site]/[...catchall]/page.tsx
 import { getSiteBySlug } from "@/lib/getSiteData";
 import { renderSite } from "@/lib/render";
 import { SiteData } from "@/lib/data";
@@ -13,8 +12,14 @@ interface PageParams {
 // Async server component page
 export default async function Page({
   params,
+  // The Next.js PageProps constraint requires `searchParams` to be present in the component props type.
+  // The original definition was missing this property, which caused the type incompatibility error.
+  // We destructure it here to satisfy the constraint, even if it's unused.
+  searchParams,
 }: {
   params: PageParams;
+  // Next.js page components require searchParams in props, even if unused in the function body
+  searchParams?: { [key: string]: string | string[] | undefined };
 }): Promise<ReactNode> {
   const { site, catchall } = params;
 
